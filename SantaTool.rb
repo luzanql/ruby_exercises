b73f5f8253a118d5236ff1178e13f02d1c4b978b
Andrea Quiceno <luz.quiceno@correounivalle.edu.co>
2020-11-12 09:40:39 -0500

    Adding Santa Tool

---
+++ SantaTool.rb
@@ -0,0 +1,11 @@
+class SantaTool
+
+	def generate_match(users)
+		#users.combination(2).to_a
+		users.shuffle.combination(2).uniq.to_a
+	end
+
+end
+
+game = SantaTool.new
+p game.generate_match(['Santa', 'Sophie', 'Claire'])
